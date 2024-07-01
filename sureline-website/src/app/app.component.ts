11e6aa024814d8652d3df5052d07fd132457031c
Jordan Hammon <hammonjordanpersonal@gmail.com>
2024-07-01 10:59:13 -0600

    created a footer navigation

--- sureline-website/src/app/app.component.ts
+++ sureline-website/src/app/app.component.ts
@@ -2,11 +2,12 @@ import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { HeaderNavigationComponent } from './header-navigation/header-navigation.component';
 import { SliderNavigationComponent } from './slider-navigation/slider-navigation.component';
+import { FooterNavigationComponent } from './footer-navigation/footer-navigation.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, HeaderNavigationComponent, SliderNavigationComponent],
+  imports: [RouterOutlet, HeaderNavigationComponent, SliderNavigationComponent, FooterNavigationComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
