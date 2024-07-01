39e543ddeee8889d9996056dbaf9317f300d6639
Jordan Hammon <hammonjordanpersonal@gmail.com>
2024-07-01 10:43:51 -0600

    created footer navigation

---
+++ sureline-website/src/app/footer-navigation/footer-navigation.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-footer-navigation',
+  standalone: true,
+  imports: [],
+  templateUrl: './footer-navigation.component.html',
+  styleUrl: './footer-navigation.component.css'
+})
+export class FooterNavigationComponent {
+
+}
