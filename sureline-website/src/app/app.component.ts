5e80553be8ae1e4c61d49bf1337c2f558624b6dc
Jordan Hammon <hammonjordanpersonal@gmail.com>
2024-06-30 21:59:01 -0600

    Added app.component.html

--- sureline-website/src/app/app.component.ts
+++ sureline-website/src/app/app.component.ts
@@ -9,5 +9,5 @@ import { RouterOutlet } from '@angular/router';
   styleUrl: './app.component.css'
 })
 export class AppComponent {
-  title = 'sureline-website';
+  title = 'Sureline Website | TurnKey Solutions';
 }
