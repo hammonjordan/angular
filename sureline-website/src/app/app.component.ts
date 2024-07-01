445b9ad8ff00aebf3ab0c6b8fdd80bd9179f312d
Jordan Hammon <hammonjordanpersonal@gmail.com>
2024-07-01 10:41:44 -0600

    added a slider navigation

--- sureline-website/src/app/app.component.ts
+++ sureline-website/src/app/app.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { HeaderNavigationComponent } from './header-navigation/header-navigation.component';
+import { SliderNavigationComponent } from './slider-navigation/slider-navigation.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, HeaderNavigationComponent],
+  imports: [RouterOutlet, HeaderNavigationComponent, SliderNavigationComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
